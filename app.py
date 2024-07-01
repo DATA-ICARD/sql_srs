4c92363f900c5e401ab563fc661a17c9660ea7ac
DATA ICARD <datayannick@gmail.com>
2024-07-01 16:02:56 +0200

    first-commit

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
 with tab1:
     sql_query = st.text_area(label="Entrez votre input")
-    result = duckdb.sql(sql_query).df()
-    st.write(f"Vous avez entré la query suivante : {sql_query}")
+    result = duckdb.query(sql_query).df()
+    st.write(f"Vous avez entré la query : {sql_query}")
     st.dataframe(result)
 
 with tab2:
